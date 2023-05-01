6cdba8e748512f50dc136bc15bd984d672df9478
Parth18062003 <85751713+Parth18062003@users.noreply.github.com>
2023-05-01 20:09:23 +0530

    Update Knapsack_Dynamic_programming.c

--- Knapsack_Dynamic_programming.c
+++ Knapsack_Dynamic_programming.c
@@ -44,3 +44,35 @@ int main()
 	printf("Maximum profit: %d", knapSack(W, weight, profit, n));
 	return 0;
 }
+
+/*function knapSack(W, wt[], val[], n)
+    // W: maximum capacity of knapsack
+    // wt[]: array of item weights
+    // val[]: array of item values
+    // n: number of items
+
+    // Initialize K[][] array to store maximum profit
+    K[n+1][W+1]
+
+    // Initialize K[][] for 0 weight or 0 item
+    for i from 0 to n do
+        K[i][0] = 0
+    end for
+    for w from 0 to W do
+        K[0][w] = 0
+    end for
+
+    // Fill the K[][] in bottom-up manner
+    for i from 1 to n do
+        for w from 1 to W do
+            if wt[i-1] <= w then
+                K[i][w] = max(val[i-1] + K[i-1][w-wt[i-1]], K[i-1][w])
+            else
+                K[i][w] = K[i-1][w]
+            end if
+        end for
+    end for
+
+    // Return the maximum profit
+    return K[n][W]
+end function*/
