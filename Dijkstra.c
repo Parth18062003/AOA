4616a8345e8e6c46939581719b34c4ea053c7fff
Parth18062003 <85751713+Parth18062003@users.noreply.github.com>
2023-05-01 21:21:11 +0530

    Update Dijkstra.c

--- Dijkstra.c
+++ Dijkstra.c
@@ -2,7 +2,7 @@
 #include <stdbool.h>
 #include <stdio.h>
 
-#define V 9
+#define V 5
 
 int minDistance(int dist[], bool sptSet[])
 {
@@ -51,15 +51,12 @@ void dijkstra(int graph[V][V], int src)
 
 void main()
 {
-    
-	int graph[V][V] = { { 0, 4, 0, 0, 0, 0, 0, 8, 0 },
-						{ 4, 0, 8, 0, 0, 0, 0, 11, 0 },
-						{ 0, 8, 0, 7, 0, 4, 0, 0, 2 },
-						{ 0, 0, 7, 0, 9, 14, 0, 0, 0 },
-						{ 0, 0, 0, 9, 0, 10, 0, 0, 0 },
-						{ 0, 0, 4, 14, 10, 0, 2, 0, 0 },
-						{ 0, 0, 0, 0, 0, 2, 0, 1, 6 },
-						{ 8, 11, 0, 0, 0, 0, 1, 0, 7 },
-						{ 0, 0, 2, 0, 0, 0, 6, 7, 0 } };
-	dijkstra(graph, 0);
+    int graph[V][V];
+    printf("Enter the weighted adjacency matrix of the graph:\n");
+    for (int i = 0; i < V; i++) {
+        for (int j = 0; j < V; j++) {
+            scanf("%d", &graph[i][j]);
+        }
+    }
+    dijkstra(graph, 0);
 }
